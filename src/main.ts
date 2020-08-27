9ecba72c00a2d7e81e91b06cb019ae86562adac7
DaCurse <dacurse0@gmail.com>
2020-08-27 22:54:29 +0300

    Fixed CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import { rateLimitConfig } from './config/rate-limit.config';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('/api');
-	app.use(rateLimit(rateLimitConfig));
 	app.enableCors();
+	app.use(rateLimit(rateLimitConfig));
 	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
