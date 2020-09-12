c400293f856c612b73e7505e8695e02caadddfd7
DaCurse <dacurse0@gmail.com>
2020-09-12 12:37:37 +0300

    Cleaned up package.json and implemented HTTP request logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { httpsOptions } from './config/https.config';
+import { morganMiddleware } from './config/morgan.config';
 import { rateLimitConfig } from './config/rate-limit.config';
-
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
@@ -12,7 +12,8 @@ async function bootstrap() {
 	app.enableCors();
 	app.use(rateLimitConfig.short);
 	app.use(rateLimitConfig.long);
-	
+	app.use(morganMiddleware);
+
 	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
