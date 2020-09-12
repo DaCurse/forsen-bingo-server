ecfc6a270a0b6305a45d0ac0299ee9ff7c8746a0
DaCurse <dacurse0@gmail.com>
2020-09-12 12:04:02 +0300

    Update packages and added rate limiting over a longer period of time

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import * as rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 import { httpsOptions } from './config/https.config';
 import { rateLimitConfig } from './config/rate-limit.config';
+
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
@@ -10,7 +10,9 @@ async function bootstrap() {
 	});
 	app.setGlobalPrefix('/api');
 	app.enableCors();
-	app.use(rateLimit(rateLimitConfig));
+	app.use(rateLimitConfig.short);
+	app.use(rateLimitConfig.long);
+	
 	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
