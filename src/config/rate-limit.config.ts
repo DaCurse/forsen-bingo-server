157486de15670e6dbf68fbbdb9168ae90eca1c57
DaCurse <dacurse0@gmail.com>
2021-06-11 17:55:53 +0300

    Improve and reduce rate limiting

--- src/config/rate-limit.config.ts
+++ src/config/rate-limit.config.ts
@@ -1,20 +1,14 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
-import * as rateLimit from 'express-rate-limit';
+import rateLimit from 'express-rate-limit';
 
 const message = new HttpException(
   'Too Many Requests',
   HttpStatus.TOO_MANY_REQUESTS
 );
 
-export const rateLimitConfig = {
-  short: rateLimit({
-    windowMs: 1000,
-    max: 5,
-    message,
-  }),
-  long: rateLimit({
-    windowMs: 1000 * 60,
-    max: 30,
-    message,
-  }),
+export const rateLimitConfig: rateLimit.Options = {
+  windowMs: 1000 * 60,
+  max: 100,
+  message: message as any,
+  skipFailedRequests: true,
 };
